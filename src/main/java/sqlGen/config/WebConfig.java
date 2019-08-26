59a7f27ac8081bff1c3a491ad781a1f3acf063a0
jimmytseng <jimmytseng@gmail.com>
2019-08-26 17:47:24 +0800

    static resource configuration

--- src/main/java/sqlGen/config/WebConfig.java
+++ src/main/java/sqlGen/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("assets/**").addResourceLocations("classpath:/assets/");
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
 	}
 
 }
