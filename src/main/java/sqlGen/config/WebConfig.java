82eaf41fb6b880bd6d00f32b4ba5ce0977724e88
jimmytseng <jimmytseng@gmail.com>
2019-08-31 16:20:00 +0800

    add tiles

--- src/main/java/sqlGen/config/WebConfig.java
+++ src/main/java/sqlGen/config/WebConfig.java
@@ -1,18 +1,35 @@
 package sqlGen.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("index");
-//		registry.addViewController("/sql/native").setViewName("sql/native");
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+//		registry.addViewController("/").setViewName("index");
+//	}
+	
+	@Bean
+	public UrlBasedViewResolver viewResolver() {
+		UrlBasedViewResolver tilesViewResolver = new UrlBasedViewResolver();
+		tilesViewResolver.setViewClass(TilesView.class);
+		return tilesViewResolver;
 	}
+	
+	@Bean
+	public TilesConfigurer tilesConfigurer() {
+		TilesConfigurer tiles = new TilesConfigurer();
+		tiles.setDefinitions(new String[] { "WEB-INF/tiles/tiles.xml" });
+		return tiles;
+	}
+
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
