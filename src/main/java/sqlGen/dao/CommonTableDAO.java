40bb444f41149623c53275d008218204a464f15d
jimmytseng <jimmytseng@gmail.com>
2019-09-12 13:06:07 +0800

    更改 DB使用方式

--- src/main/java/sqlGen/dao/CommonTableDAO.java
+++ src/main/java/sqlGen/dao/CommonTableDAO.java
@@ -19,7 +19,7 @@ public class CommonTableDAO {
 	private JdbcTemplate jdbcTemplate;
 
 	public Map<String, String> getAllTableOption() {
-		String queryString = "SELECT [TABLE_NAME] FROM [INFORMATION_SCHEMA].[TABLES]";
+		String queryString = "SELECT [TABLE_NAME] FROM [INFORMATION_SCHEMA].[TABLES] ORDER BY [TABLE_NAME] ";
 		return this.jdbcTemplate.query(queryString, new ResultSetExtractor<Map<String, String>>() {
 			@Override
 			public Map<String, String> extractData(ResultSet rs) throws SQLException, DataAccessException {
@@ -33,10 +33,11 @@ public class CommonTableDAO {
 	}
 
 	public List<String> getTableColumns(String tableName) {
-		StringBuilder queryBuilder = new StringBuilder(" SELECT [COLUMN_NAME] \n");
-		queryBuilder.append(" FROM [TestDB].[INFORMATION_SCHEMA].[COLUMNS] \n");
-		queryBuilder.append("  where [TABLE_NAME] = ? ");
-		return this.jdbcTemplate.query(queryBuilder.toString(), new String[] { tableName },
+//		StringBuilder queryBuilder = new StringBuilder(" SELECT [COLUMN_NAME] \n");
+//		queryBuilder.append(" FROM [TestDB].[INFORMATION_SCHEMA].[COLUMNS] \n");
+//		queryBuilder.append("  where [TABLE_NAME] = ? ");
+		StringBuilder queryBuilder =new StringBuilder("SELECT name FROM sys.dm_exec_describe_first_result_set(?, null, 1)");
+		return this.jdbcTemplate.query(queryBuilder.toString(), new String[] { "SELECT * FROM dbo."+ tableName},
 				new int[] { java.sql.Types.VARCHAR }, new ResultSetExtractor<List<String>>() {
 					@Override
 					public List<String> extractData(ResultSet rs) throws SQLException, DataAccessException {
